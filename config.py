a07dda4393275c57391b4a921418fb9f0aab4027
Vadim <85992119+lolopogff@users.noreply.github.com>
2025-09-27 16:05:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -29,6 +29,6 @@ class Settings:
     """
     Хранение ключей для доступа к Yandex GPT API
     """
-    FOLDER_ID = "b1gv4c52iaki869acqjb"
-    API_KEY = "AQVN00Kh_26JTJUCXCJ7AMjj49QuaRrFRelpISkt"
-    IAM_TOKEN = "AQVN0tNAgGOR7JkXS8s95SW2-qFUgWHVn5PEp9Jz"
+    FOLDER_ID = "FOLDER_ID"
+    API_KEY = "API_KEY"
+    IAM_TOKEN = "IAM_TOKEN"
